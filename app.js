5d64cc2d89a0a1b4319265979b7b976842a956d1
Jalal Mohamed <jalalmohamed2702@gmail.com>
2024-08-13 17:05:35 +0530

    Lets go live

--- app.js
+++ app.js
@@ -9,9 +9,25 @@ const Expense=require('./models/expense');
 const Order = require('./models/orders');
 const ForgetPassword = require('./models/ForgotPasswordRequests');
 require('dotenv').config();
+const helmet = require('helmet');
+const express = require('express');
+app.use(helmet());
+
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+
+app.use(morgan('combined', { stream: accessLogStream }));
 
 const app=express();
 
+app.use((err, req, res, next) => {
+    logger.error(err.message, { stack: err.stack });
+    res.status(500).send('Something went wrong!');
+  });
+  
 app.use(bodyparser.urlencoded({ extended: false}));
 
 app.use(bodyparser.json());
