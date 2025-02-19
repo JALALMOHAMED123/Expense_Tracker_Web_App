81abbde8900c45d505d3c189d74b51439f230e35
Jalal Mohamed <jalalmohamed2702@gmail.com>
2025-02-19 12:12:23 +0530

    removing unwanted files

--- util/signup.js
+++
@@ -1,8 +0,0 @@
-const Sequelize=require('sequelize');
-
-const sequelize=new Sequelize('expense', 'root', 'Jalal@2024', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
-
-module.exports=sequelize;
